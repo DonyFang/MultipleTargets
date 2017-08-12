b5ca0e0de7ca08ae52e1a28d9bcce609a7a52354
DongFang <Fang_DongDong@want-want.com>
2017-08-12 11:01:51 +0800

    commit

---
+++ CoupleTarget/CoupleTarget/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  CoupleTarget
+//
+//  Created by 方冬冬 on 2017/8/12.
+//  Copyright © 2017年 方冬冬. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Macros.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"%@",BASE_URL);
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CoupleTarget/Macros.h
@@ -0,0 +1,19 @@
+//
+//  Macros.h
+//  CoupleTarget
+//
+//  Created by 方冬冬 on 2017/8/12.
+//  Copyright © 2017年 方冬冬. All rights reserved.
+//
+
+#if CoupleTarget //fir test domain
+
+#define BASE_URL @"http://www.CoupleTarget.com"
+#define DATABASE_NAME @"svm_test10"
+#define CHANNEL @"Enterprise"
+
+#elif CoupleTarget_beta //fir pre domain
+#define BASE_URL @"http://www.CoupleTarget_beta.com"
+#define DATABASE_NAME @"svmpre"
+#define CHANNEL @"Enterprise"
+#endif
